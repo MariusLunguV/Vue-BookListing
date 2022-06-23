75d2f3181f0f33fbfe4906ccde861f10c5b593fb
Marius Lungu <marius.lungu@valtech.com>
2022-06-23 11:57:56 +0200

    complete module3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="book in books" :key="book">
+      {{ book.title }}:{{ book.author }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"],
+};
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <ul>
-      <li v-for='book in books' :key="book">{{book.title}}:{{book.author}}</li>
-    </ul>
+    <ul></ul>
   </div>
 </template>
 
@@ -33,8 +31,5 @@ ul{
   list-style-type: none;
   padding: 0;
 }
-li{
-  display: block;
-  margin: 0 10px;
-}
+
 </style>
